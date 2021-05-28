4a49e432ce45ad04b1bc53c8043e5a9749b3c48b
Aleksandr Polonnikov <kansk.sasha@live.com>
2021-05-28 23:21:01 +0700

    first ver

--- arif_codir.cpp
+++ arif_codir.cpp
@@ -2,9 +2,9 @@
 #include <fstream>
 #include <cstddef>
 #include <map>
+#include <iomanip>
 constexpr short int SIZE_OF_CHARS = 15;
 using namespace std;
-
 
 int main()
 {
@@ -13,7 +13,7 @@ int main()
     map <unsigned char, int> frequency;
     unsigned char c = 0;
     unsigned long long count = 0;
-
+    
     //reading a file and counting the character frequencies in an array
     while ((c = fin.get()) && !fin.eof())
     {
@@ -24,31 +24,26 @@ int main()
     //translating the pointer to the beginning of the file
     fin.clear();
     fin.seekg(0);
-    //fout.write((char*) count, sizeof(count));
-    //fout.write((char*) SIZE_OF_CHARS, sizeof(SIZE_OF_CHARS));
-    //fout.write((char*) int(frequency.size()), sizeof(int(frequency.size())));
-    fout << count << short int(SIZE_OF_CHARS) << int(frequency.size());
+    fout << count << " " << short int(SIZE_OF_CHARS) << " " << int(frequency.size());
 
-    map<unsigned char, double*> freq;
-    double a = 0, b = 0;
+    map <unsigned char, double long*> freq;
+    double long a = 0, b = 0;
     for (auto s : frequency)
     {
-        fout << s.first << s.second;
-        //fout.write((char*)s.first, sizeof(s.first));
-        //fout.write((char*)s.second, sizeof(s.second));
-        freq[s.first] = new double[3];
-        freq[s.first][0] = double(s.second) / double(count);
+        fout << s.first << s.second << " ";
+        freq[s.first] = new double long[3];
+        freq[s.first][0] = double long(s.second) / double long(count);
         b = a + freq[s.first][0];
         freq[s.first][1] = a;
         freq[s.first][2] = b;
         a = b;
     }
 
-    double l = 0, h = 1;
+    double long l = 0, h = 1;
     int i = 0;
     while ((c = fin.get()) && !fin.eof())
     {
-        double _l = l + freq[c][1] * (h - l),
+        double long _l = l + freq[c][1] * (h - l),
             _h = l + freq[c][2] * (h - l);
         l = _l;
         h = _h;
@@ -56,7 +51,7 @@ int main()
 
         if (i % SIZE_OF_CHARS == 0)
         {
-            fout << l;
+            fout << setprecision(100) << l;
             l = 0;
             h = 1;
         }
@@ -64,7 +59,7 @@ int main()
 
     if (i % SIZE_OF_CHARS)
     {
-        fout << l;
+        fout << setprecision(100) << l;
     }
 
     return 0;
