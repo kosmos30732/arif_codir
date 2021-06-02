296f90db32c6b6fde62035da03246fad06cc28ea
Aleksandr Polonnikov <kansk.sasha@live.com>
2021-06-02 21:01:23 +0700

    add binary

--- arif_codir.cpp
+++ arif_codir.cpp
@@ -2,127 +2,70 @@
 #include <fstream>
 #include <cstddef>
 #include <map>
-#include <iomanip>
-#include <string>
-#include <sstream>
 constexpr short int SIZE_OF_CHARS = 8;
 using namespace std;
 
 int main()
 {
-    ifstream fin("input.txt");
-    ofstream fout("output.txt");
-    map <unsigned char, int> frequency;
-    unsigned char c = 0;
-    unsigned long long count = 0;
-    
-    //reading a file and counting the character frequencies in an array
-    while ((c = fin.get()) && !fin.eof())
-    {
-        frequency[c]++;
-        count++;
-    }
+	ifstream fin("input.txt");
+	ofstream fout("output.txt", ios::binary);
+	map <unsigned char, unsigned int> frequency;
+	unsigned char c = 0;
+	unsigned long long count = 0;
 
-    //translating the pointer to the beginning of the file
-    fin.clear();
-    fin.seekg(0);
-    fout << count << " " << short int(SIZE_OF_CHARS) << " " << int(frequency.size());
+	//reading a file and counting the character frequencies in an array
+	while ((c = fin.get()) && !fin.eof())
+	{
+		frequency[c]++;
+		count++;
+	}
 
-    map <unsigned char, double long*> freq;
-    double long a = 0, b = 0;
-    for (auto s : frequency)
-    {
-        fout << s.first << s.second << " ";
-        freq[s.first] = new double long[3];
-        freq[s.first][0] = double long(s.second) / double long(count);
-        b = a + freq[s.first][0];
-        freq[s.first][1] = a;
-        freq[s.first][2] = b;
+	//translating the pointer to the beginning of the file
+	fin.clear();
+	fin.seekg(0);
 
-        //cout << s.first << " ";
-        //cout.width(10);
-        //cout << freq[s.first][0] << " ";
-        //cout.width(10);
-        //cout << freq[s.first][1] << " ";
-        //cout.width(10);
-        //cout << freq[s.first][2] << endl;
+	fout.write((char*)&count, sizeof(count));
+	fout.write((char*)&SIZE_OF_CHARS, sizeof(SIZE_OF_CHARS));
+	unsigned int FreqSize = frequency.size();
+	fout.write((char*)&FreqSize, sizeof(FreqSize));
 
-        a = b;
-    }
+	map <unsigned char, double*> freq;
+	double a = 0, b = 0;
+	for (auto s : frequency)
+	{
+		fout.write((char*)&s.first, sizeof(s.first));
+		fout.write((char*)&s.second, sizeof(s.second));
 
-    double long l = 0, h = 1;
-    int i = 0;
-    while ((c = fin.get()) && !fin.eof())
-    {
-        double long _l = l + freq[c][1] * (h - l),
-            _h = l + freq[c][2] * (h - l);
-        l = _l;
-        h = _h;
-        i++;
-        //cout << endl;
-        //cout << "Encode: " << i << endl;
-        //cout << "l: " << setprecision(100) << l << endl;
-        //cout << "h: " << setprecision(100) << h << endl;
-        //cout << endl;
+		freq[s.first] = new double[3];
+		freq[s.first][0] = double(s.second) / double(count);
+		b = a + freq[s.first][0];
+		freq[s.first][1] = a;
+		freq[s.first][2] = b;
+		a = b;
+	}
 
-        if (i % SIZE_OF_CHARS == 0)
-        {
-            stringstream s1, s2;
-            s1 << setprecision(100) << l;
-            s2 << setprecision(100) << h;
-            if (s1.str()!=s2.str())
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    if (s1.str()[i] != s2.str()[i])
-                    {
-                        char buf[100];
-                        auto len = s1.str().copy(buf, i+1, 0);
-                        buf[len - 1]++;
-                        buf[len] = '\0';
-                        fout << buf;
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                fout << s1.str();
-            }
-            
-            //fout << l;
-            l = 0;
-            h = 1;
-            i = 0;
-        }
-    }
+	double l = 0, h = 1;
+	int i = 0;
+	while ((c = fin.get()) && !fin.eof())
+	{
+		double _l = l + freq[c][1] * (h - l),
+			_h = l + freq[c][2] * (h - l);
+		l = _l;
+		h = _h;
+		i++;
 
-    if (i % SIZE_OF_CHARS)
-    {
-        stringstream s1, s2;
-        s1 << setprecision(100) << l;
-        s2 << setprecision(100) << h;
-        if (s1.str() != s2.str())
-        {
-            for (int i = 0; i < 100; i++)
-            {
-                if (s1.str()[i] != s2.str()[i])
-                {
-                    char buf[100];
-                    auto len = s1.str().copy(buf, i + 1, 0);
-                    buf[len - 1]++;
-                    buf[len] = '\0';
-                    fout << buf;
-                    break;
-                }
-            }
-        }
-        else
-        {
-            fout << s1.str();
-        }
+		if (i % SIZE_OF_CHARS == 0)
+		{
+			fout.write(reinterpret_cast<char*>(&l), sizeof(l));
+			l = 0;
+			h = 1;
+			i = 0;
+		}
+	}
 
-    }
-
-    return 0;
+	if (i % SIZE_OF_CHARS)
+	{
+		fout.write(reinterpret_cast<char*>(&l), sizeof(l));
+	}
+	return 0;
 }
